7b05d1fd7d909c4ebacde07cf0543f8579440ec0
Ubuntu <ubuntu@ip-172-31-45-218.ap-south-1.compute.internal>
2026-02-11 10:28:03 +0000

    added code files

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM eclipse-temurin:17-jre
+
+WORKDIR /app
+COPY target/trivy-demo-0.0.1.jar app.jar
+
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/com/demo/app.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from CICD";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
+
