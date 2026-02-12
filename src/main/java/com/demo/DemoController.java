8dd468ba108afe664cf30d3f92a59c07377b9cd6
Ubuntu <ubuntu@ip-172-31-45-218.ap-south-1.compute.internal>
2026-02-12 12:28:20 +0000

    added Democontroller.java file to force create a bug in code scan

---
+++ src/main/java/com/demo/DemoController.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/test")
+    public String test() {
+        String s = null;
+        return s.toString();  // NullPointerException risk
+    }
+}
+
